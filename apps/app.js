5b26f84097ede4b7c55da054fa8315784af9b443
gfed53 <gfederico7153@gmail.com>
2016-02-11 12:20:23 -0500

    Search working, pre-view, results 'console.logged'

--- apps/app.js
+++ apps/app.js
@@ -1,4 +1,35 @@
-app.module("FlickrApp", [])
+angular.module("FlickrApp", [])
 .controller("FlickrCtrl", ['$scope', '$timeout', '$q', '$http', function($scope, $timeout, $q, $http){
 
+	$scope.searchFlickr = function(tag){
+		console.log("searching");
+		$scope.tag = tag;
+		$scope.notifySearch = true;
+		var url = "https://api.flickr.com/services/rest",
+		params = {
+			method: 'flickr.photos.search',
+			api_key: "a35c104c1a7f9762e0f6cdf064f39657",
+			tags: tag,
+			format: 'json',
+			nojsoncallback: 1
+		};
+
+		$http({
+			method: 'GET',
+			url: url,
+			params: params
+		})
+		.then(function(response){
+			$scope.notifySearch = false;
+			$scope.notifyResults = true;
+			console.log(response.data.photos.photo);
+			// $scope.results = response.data.items;
+		},
+		function(response){
+			alert('error');
+			$scope.error = true;
+		});
+
+
+	};
 }]);
