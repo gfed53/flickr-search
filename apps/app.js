a6421c9ff43749887d27bee2203585c56ff7cdbe
gfed53 <gfederico7153@gmail.com>
2016-06-09 11:10:31 -0400

    Code cleanup

--- apps/app.js
+++ apps/app.js
@@ -1,29 +1,19 @@
-angular.module("FlickrApp", ['ngAnimate'])
+angular.module('FlickrApp', ['ngAnimate'])
 
-.controller("FlickrCtrl", ['$timeout', '$q', '$http', function($timeout, $q, $http){
+.controller('FlickrCtrl', ['$timeout', '$q', '$http', function($timeout, $q, $http){
 	var vm = this;
 	vm.initMap = initMap;
 	vm.searchFlickr = searchFlickr;
 
 	vm.initMap();
-	// vm.north = 33;
-	// vm.south = 42;
-	// vm.east = -91;
-	// vm.west = -107;
-	// console.log(vm.map);
 
 	function initMap() {
-        vm.map = new google.maps.Map(document.getElementById('map'), {
+        vm.map = new google.maps.Map(document.getElementById("map"), {
           center: {lat: 39, lng: -99},
           zoom: 2
-          	// center: {lat: 40, lng: -73},
-          	// zoom: 8
         });
+        
         var bounds = {
-		    // north: 33,
-		    // south: 42,
-		    // east: -91,
-		    // west: -107
 		    north: 42,
 		    south: 37,
 		    east: -88,
@@ -37,23 +27,13 @@ angular.module("FlickrApp", ['ngAnimate'])
 		});
 
 		vm.rectangle.setMap(vm.map);
-        console.log(vm.map);
-        console.log(vm.rectangle);
         vm.rectangle.addListener("bounds_changed", function(){
-        	//Obj order: south, north, east, west?
         	var bounds = vm.rectangle.getBounds();
         	vm.south = bounds.H.H;
         	vm.north = bounds.H.j;
         	vm.east = bounds.j.H;
         	vm.west = bounds.j.j;
-        	console.log(vm.south);
-        	console.log(vm.north);
-        	console.log(vm.east);
-        	console.log(vm.west);
-
         });
-
-
       }
 
 	function searchFlickr(tag) {
@@ -64,8 +44,6 @@ angular.module("FlickrApp", ['ngAnimate'])
 		vm.error = false;
 		vm.notifySearch = true;
 
-		// initMap();
-
 		var url = "https://api.flickr.com/services/rest",
 		params = {
 			method: "flickr.photos.search",
@@ -73,26 +51,23 @@ angular.module("FlickrApp", ['ngAnimate'])
 			tags: "outdoor, -people, -portrait, "+ tag,
 			tag_mode: "all",
 			bbox: vm.west+", "+vm.south+", "+vm.east+", "+vm.north,
-			// bbox: [vm.west, vm.south, vm.east, vm.north],
 			safe_search: 1,
 			format: "json",
 			nojsoncallback: 1
 		};
 
 		$http({
-			method: 'GET',
+			method: "GET",
 			url: url,
 			params: params
 		})
 		.then(function(response){
 			vm.notifySearch = false;
 			vm.notifyResults = true;
-			vm.results = response.data.photos.photo;
-			console.log(vm.results);
-			
+			vm.results = response.data.photos.photo;	
 		},
 		function(response){
-			alert('Sorry, an error occurred.');
+			alert("Sorry, an error occurred.");
 			vm.error = true;
 		})
 		.then(function(){
@@ -101,15 +76,4 @@ angular.module("FlickrApp", ['ngAnimate'])
 	};
 }])
 
-.run(function(){
-	// var map;
-	// function initMap() {
- //        map = new google.maps.Map(document.getElementById('map'), {
- //          center: {lat: 40, lng: 73},
- //          zoom: 8
- //        });
- //      }
 
- //    initMap();
- //    console.log(map);
-})
